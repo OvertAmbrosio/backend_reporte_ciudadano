cc92d47479edc319f42a6661686440ce5e7adb1f
Overt Ambrosio <overt.ambrosio@culqi.com>
2026-02-14 17:04:31 -0500

    feat: se extiende el tamaño para las imagenes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors(); // Habilitar CORS para acceso publico
